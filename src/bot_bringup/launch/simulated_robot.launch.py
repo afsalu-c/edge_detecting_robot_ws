d3f62dfc859c26fd98bfd5cd572fd5a1b88d6b2b
Afsalu Rahman C <afsalu2002@gmail.com>
2025-07-29 15:00:09 +0530

    after all step but only able to teleoperate with following commands [ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -p stamped:=true -r /cmd_vel:=wheel_controller/cmd_vel] and [ros2 topic pub -r5 /wheel_controller/cmd_vel geometry_msgs/msg/TwistStamped {

---
+++ src/bot_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,62 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    world_name = LaunchConfiguration("world_name")
+
+    world_name_arg = DeclareLaunchArgument(
+        "world_name",
+        default_value="custom_eight_table_world"
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory("bot_description"),
+            "launch",
+            "gazebo.launch.py"
+        )),
+        launch_arguments={
+            "world_name":world_name
+        }.items()
+    )
+    controller_only = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory("bot_controller"),
+                "launch",
+                "controller.launch.py"
+            ))
+        )
+    
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(          #"-d" is a command-line flag for rviz2, -d stands for "display config file
+            get_package_share_directory("bot_description"), #ex: rviz2 -d ~/ros2_ws/src/bot_description/rviz/display.rviz
+            "rviz",
+            "display.rviz"
+        )],
+        output="screen",
+        parameters=[{"use_sim_time": True}]
+    )
+
+    edge_avoidance_node = Node(
+        package="bot_script",
+        executable="edge_detection",
+        name="edge_avoidance_node",
+        output="screen", # ← This will let you see log output in terminal, if output="log" = save logs to files only
+        parameters=[{"use_sim_time": True}]
+    )
+
+    return LaunchDescription([
+        world_name_arg,
+        gazebo,
+        controller_only,
+        rviz,
+        #edge_avoidance_node,
+        
+    ])
---
+++ src/bot_script/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'bot_script'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='afsalu-rahman-c',
+    maintainer_email='afsalu2002@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "edge_detection = bot_script.edge_detection:main",        ],
+    },
+)
